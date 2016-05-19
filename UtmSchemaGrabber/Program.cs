4f74561c84f1f5d4fe2e9a554db934d8682252d5
Denis Zimin <denissio@land.ru>
2016-05-19 22:51:04 +0300

    Initial commit

--- UtmSchemaGrabber/Program.cs
+++ UtmSchemaGrabber/Program.cs
@@ -1,15 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace UtmSchemaGrabber
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var client = new HttpClient();
+            var regexList = new Regex("\\.load\\('info/(?<Folder>.+?)/(?<Filename>(?:.+?)(?:'|\\.xsd))",
+                    RegexOptions.IgnoreCase
+                    | RegexOptions.Multiline
+                    | RegexOptions.Singleline
+                    | RegexOptions.CultureInvariant
+                    | RegexOptions.IgnorePatternWhitespace
+                    | RegexOptions.Compiled
+                    );
+
+            var regexPre = new Regex("<pre>(.*?)</pre>",
+                    RegexOptions.IgnoreCase
+                    | RegexOptions.Multiline
+                    | RegexOptions.Singleline
+                    | RegexOptions.CultureInvariant
+                    | RegexOptions.IgnorePatternWhitespace
+                    | RegexOptions.Compiled
+                    );
+
+            try
+            {
+                var mainPage = client.GetStringAsync(new Uri(args[0])).GetAwaiter().GetResult();
+
+                var matches = regexList.Matches(WebUtility.HtmlDecode(mainPage));
+
+                var listOfFiles = matches.Cast<Match>()
+                    .Where(x => x.Groups["Filename"].Value.EndsWith(".xsd", StringComparison.InvariantCultureIgnoreCase))
+                    .Select(x => new
+                    {
+                        Url = $"{args[0]}/info/{x.Groups["Folder"]}/{x.Groups["Filename"]}",
+                        FileName = $@"\{x.Groups["Folder"]}\{x.Groups["Filename"]}"
+                    })
+                    .ToList();
+
+                var rootDir = Directory.CreateDirectory("EgaisXsd");
+                var subDirs = matches.Cast<Match>()
+                    .Select(x => x.Groups["Folder"].Value)
+                    .Distinct()
+                    .Where(x => x != "db" && x != "certificate");
+
+                foreach (var folder in subDirs)
+                {
+                    Directory.CreateDirectory($@"{rootDir.FullName}\{folder}");
+                }
+
+                foreach (var file in listOfFiles)
+                {
+                    var html = client.GetStringAsync(file.Url).GetAwaiter().GetResult();
+                    var match = regexPre.Match(html).Groups[1].Value;
+                    File.WriteAllText(rootDir.FullName + file.FileName, WebUtility.HtmlDecode(match));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
