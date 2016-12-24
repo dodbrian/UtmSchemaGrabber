2eaf65cc2a5cbf004c39b6cdcab518af3b1544d2
Denis Zimin <roydronov@gmail.com>
2016-12-24 19:55:07 +0300

    Вывод подробного сообщения об ошибках

--- UtmSchemaGrabber/Program.cs
+++ UtmSchemaGrabber/Program.cs
@@ -3,17 +3,22 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace UtmSchemaGrabber
 {
     internal class Program
     {
+        public static string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
         private static void Main(string[] args)
         {
+            Console.WriteLine($"UtmSchemaGrabber, Версия {AssemblyVersion}\n");
+
             if (args.Length < 1)
             {
-                Console.WriteLine("\nИспользование: UtmSchemaGrabber.exe http://<UtmAddress>:<port>");
+                Console.WriteLine("Использование: UtmSchemaGrabber.exe http://<UtmAddress>:<port>");
                 Console.WriteLine("где:\n\t<UtmAddress> - полный адрес УТМ");
                 Console.WriteLine("\t<port> - порт, на котором работает УТМ\n");
                 Console.WriteLine("Пример: UtmSchemaGrabber.exe http://localhost:8080");
@@ -21,7 +26,6 @@ namespace UtmSchemaGrabber
                 return;
             }
 
-            var client = new HttpClient();
             var regexList = new Regex("\\.load\\('info/(?<Folder>.+?)/(?<Filename>(?:.+?)(?:'|\\.xsd))",
                     RegexOptions.IgnoreCase
                     | RegexOptions.Multiline
@@ -42,40 +46,56 @@ namespace UtmSchemaGrabber
 
             try
             {
-                var mainPage = client.GetStringAsync(new Uri(args[0])).GetAwaiter().GetResult();
+                using (var client = new HttpClient())
+                {
+                    var mainPage = client.GetStringAsync(new Uri(args[0])).GetAwaiter().GetResult();
+                    var matches = regexList.Matches(WebUtility.HtmlDecode(mainPage));
 
-                var matches = regexList.Matches(WebUtility.HtmlDecode(mainPage));
-
-                var listOfFiles = matches.Cast<Match>()
-                    .Where(x => x.Groups["Filename"].Value.EndsWith(".xsd", StringComparison.InvariantCultureIgnoreCase))
-                    .Select(x => new
+                    if (matches.Count == 0)
                     {
-                        Url = $"{args[0]}/info/{x.Groups["Folder"]}/{x.Groups["Filename"]}",
-                        FileName = $@"\{x.Groups["Folder"]}\{x.Groups["Filename"]}"
-                    })
-                    .ToList();
+                        Console.WriteLine("По заданной ссылке не найдено ни одной схемы XSD");
+                        return;
+                    }
 
-                var rootDir = Directory.CreateDirectory("EgaisXsd");
-                var subDirs = matches.Cast<Match>()
-                    .Select(x => x.Groups["Folder"].Value)
-                    .Distinct()
-                    .Where(x => x != "db" && x != "certificate");
+                    var listOfFiles = matches.Cast<Match>()
+                        .Where(x => x.Groups["Filename"].Value.EndsWith(".xsd", StringComparison.InvariantCultureIgnoreCase))
+                        .Select(x => new
+                        {
+                            Url = $"{args[0]}/info/{x.Groups["Folder"]}/{x.Groups["Filename"]}",
+                            FileName = $@"\{x.Groups["Folder"]}\{x.Groups["Filename"]}"
+                        })
+                        .ToList();
 
-                foreach (var folder in subDirs)
-                {
-                    Directory.CreateDirectory($@"{rootDir.FullName}\{folder}");
-                }
+                    var rootDir = Directory.CreateDirectory("EgaisXsd");
 
-                foreach (var file in listOfFiles)
-                {
-                    var html = client.GetStringAsync(file.Url).GetAwaiter().GetResult();
-                    var match = regexPre.Match(html).Groups[1].Value;
-                    File.WriteAllText(rootDir.FullName + file.FileName, WebUtility.HtmlDecode(match));
+                    var subDirs = matches.Cast<Match>()
+                        .Select(x => x.Groups["Folder"].Value)
+                        .Distinct()
+                        .Where(x => x != "db" && x != "certificate");
+
+                    foreach (var folder in subDirs)
+                    {
+                        Directory.CreateDirectory($@"{rootDir.FullName}\{folder}");
+                    }
+
+                    Console.WriteLine("Сохранение файлов...\n");
+
+                    foreach (var file in listOfFiles)
+                    {
+                        Console.WriteLine(file.FileName);
+                        var html = client.GetStringAsync(file.Url).GetAwaiter().GetResult();
+                        var match = regexPre.Match(html).Groups[1].Value;
+                        File.WriteAllText(rootDir.FullName + file.FileName, WebUtility.HtmlDecode(match));
+                    }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                do
+                {
+                    Console.WriteLine(ex.Message);
+                    ex = ex.InnerException;
+                } while (ex != null);
             }
         }
     }
